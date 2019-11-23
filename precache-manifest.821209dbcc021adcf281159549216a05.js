91a84eeb04ebb16bd3af8cd32eadc676c753aca7
Anna Kholod <uashop89@gmail.com>
2019-11-23 14:06:09 +0200

    Updates

---
+++ precache-manifest.821209dbcc021adcf281159549216a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cfb21d7bf4c2d055cf8a190b4b4442",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "3057fd13f52d15e3d13c",
+    "url": "/goit-react-hw-05-budget-planner/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a6135a00782c5fcb02f2",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "3057fd13f52d15e3d13c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.7032b306.chunk.js"
+  },
+  {
+    "revision": "a6135a00782c5fcb02f2",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.bb4cd289.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
